d6838bb637ed439e88fa78fa52fedfc324b7e6ef
BKaric <b_kari02@uni-muenster.de>
2017-11-07 12:34:16 +0100

    new TestUI

---
+++ public/javascripts/index.js
@@ -0,0 +1,14 @@
+/**
+ * Geosoftware I, SoSe 2017, final
+ * @author Jan Speckamp (428367)
+ */
+/**
+ * Loads all requested Stages. Overwrites all Submit Handlers that are already present,
+ */
+$(document).ready(function() {
+    initMap();
+    // Show all Items when initially opening Search Bar
+    sidebar.on('content', function(e) {
+    });
+
+});
