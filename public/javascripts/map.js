0b1de9c4da3af8f9264c08d3bf277c94fb189afe
Jasper Buss <jasper_buss@Jaspers-MBP.fritz.box>
2018-01-09 22:30:04 +0100

    basemaps

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -46,7 +46,14 @@ function initMap() {
       '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
   }).addTo(map);
 
-  var basemaps = { OpenStreetMap: osm };
+  var OpenTopoMap = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+  	maxZoom: 17,
+  	attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  var basemaps = { OpenStreetMap: osm,
+                  "Topographie" : OpenTopoMap
+};
   var overlaymaps = {};
 
   layerControl = L.control.layers(basemaps, overlaymaps, { collapsed: false }).addTo(map);
