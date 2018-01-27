d59cf3639d28af85eb3284201259ead7da3b3dd1
Jens Seifert <jense.92+github@gmail.com>
2018-01-27 16:20:16 +0100

    Fix for merge conflict

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -82,7 +82,7 @@ function initMap() {
   map.on('click', function(e) {
     var coords = {lat: e.latlng.lat, lng:correctCoordinates(e.latlng.lng)};
     console.log(coords);
-    //console.log(jsonForDatasets);
+    console.log(jsonForDatasets);
   });
 
  map.on('draw:created', function(e) {
